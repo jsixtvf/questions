de0406243d0dfff7bd107f3fe75ece36794bae44
jsixtvf <jsixtvf@hotmail.com>
2017-02-18 20:12:55 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,77 @@
+var formElement=null;
+//var numeroSecreto=null;
+var respescrita=null;
+var respuestaSelect=null;
+var respuestasCheckbox = [];
+var nota = 0;  //nota de la prueba sobre 3 puntos (hay 3 preguntas)
+
+
+window.onload = function(){ 
+
+ //CORREGIR al apretar el botón
+ formElement=document.getElementById('myform');
+ formElement.onsubmit=function(){
+   inicializar();
+   corregirNumber();
+   corregirSelect();
+   corregirCheckbox();
+   presentarNota();   
+   return false;
+ }
+ 
+ //LEER XML de xml/preguntas.xml
+ var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   gestionarXml(this);
+  }
+ };
+ xhttp.open("GET", "xml/questions.xml", true);
+ xhttp.send();
+}
+
+// poner los datos recibios en el HTML
+function ponerDatosInputHtml(t){
+ document.getElementById('tituloInput').innerHTML = t;
+}
+
+ function ponerDatosSelectHtml(t,opt){
+  document.getElementById("tituloSelect").innerHTML=t;
+  var select = document.getElementsByTagName("select")[0];
+  for (i = 0; i < opt.length; i++) { 
+    var option = document.createElement("option");
+    option.text = opt[i];
+    option.value=i+1;
+    select.options.add(option);
+ }  
+}
+
+function ponerDatosCheckboxHtml(t,opt){
+ var checkboxContainer=document.getElementById('checkboxDiv');
+ document.getElementById('tituloCheckbox').innerHTML = t;
+ for (i = 0; i < opt.length; i++) { 
+    var input = document.createElement("input");
+    var label = document.createElement("label");
+    label.innerHTML=opt[i];
+    label.setAttribute("for", "color_"+i);
+    input.type="checkbox";
+    input.name="color";
+    input.id="color_"+i;;    
+    checkboxContainer.appendChild(input);
+    checkboxContainer.appendChild(label);
+ }  
+} 
+
+function gestionarXml(dadesXml){
+ var xmlDoc = dadesXml.responseXML; //Parse XML to xmlDoc
+ 
+ //NUMBER
+ //Recuperamos el título y la respuesta correcta de Input, guardamos el número secreto
+ var tituloInput=xmlDoc.getElementsByTagName("title")[0].innerHTML;
+ ponerDatosInputHtml(tituloInput);
+ respescrita=parseInt(xmlDoc.getElementsByTagName("answer")[0].innerHTML);
+ 
+ function inicializar(){
+   document.getElementById('resultadosDiv').innerHTML = "";
+   nota=0.0;
+}
