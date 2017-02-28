71da2ea5129cd490f975959589e4ed09dffb8cdb
jsixtvf <jsixtvf@hotmail.com>
2017-02-28 22:27:26 +0100

    Update js.js

--- js.js
+++ js.js
@@ -207,7 +207,7 @@ function ponerDatosRadioHtml(t,opt){
 
 function ponerDatosMultipleHtml(t,opt){
   document.getElementById("tituloMultiple").innerHTML=t;
-  var select = document.getElementsByTagName("select")[1];
+  var select = document.getElementsByTagName("select")[0];
   for (i = 0; i < opt.length; i++) { 
     var option = document.createElement("option");
     option.text = opt[i];
