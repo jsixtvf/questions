0a0af5983924e074f87cf51d25043786a5d82942
jsixtvf <jsixtvf@hotmail.com>
2017-03-01 01:19:44 +0100

    Update js.js

--- js.js
+++ js.js
@@ -106,7 +106,7 @@ var nres = xmlDoc.getElementById("jklm_001").getElementsByTagName('answer').leng
 function corregirNumber(){
   //Vosotros debéis comparar el texto escrito con el texto que hay en el xml
   //en este ejemplo hace una comparación de números enteros
-  var s=formElement.elements[0].text;     
+  var s=formElement.elements[0].textContent;     
   if (s==respCorrecta) {
    darRespuestaHtml("P1: Exacto!");
    nota +=1;
