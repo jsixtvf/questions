b6bd07352add7cbf9e7f4e68f19a56c3178c6429
jsixtvf <jsixtvf@hotmail.com>
2017-03-01 01:11:16 +0100

    Update js.js

--- js.js
+++ js.js
@@ -14,10 +14,10 @@ window.onload = function(){
  formElement=document.getElementById('myform');
  formElement.onsubmit=function(){
    inicializar();
-  /* corregirNumber();
+   corregirNumber();
    corregirSelect();
    corregirCheckbox();
-   presentarNota(); */
+  /* presentarNota(); */
    return false;
  }
  
